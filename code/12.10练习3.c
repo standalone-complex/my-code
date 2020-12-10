2156b343a709b7bac6757d26ff328c506fd6451e
tyf <1552068173@qq.com>
2020-12-10 20:23:36 +0800

    git使用练习加代码能力练习

--- code/12.10练习3.c
+++ code/12.10练习3.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 
-int f(int);
+long long f(long long);
+long long g(long long);
 
 int main()
 {
-    int n;
-    printf("%d", f(n));
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld.00", g(n));
     return 0;
 }
 
-int f(int n)
+long long f(long long n)
 {
-    if(n==1||n==2)
+    if(n==1|n==2)
     return 1;
     return f(n-1) + f(n-2);
+}
+
+long long g(long long n)
+{
+    long long i, x1=1, x2 = 1, x3;
+    if(n==1||n==2)
+    {
+        return 1;
+    }
+    else if(n==0)
+    {
+        return 0;
+    }
+    else
+    {
+        for(i=0; i<n-2; i++)
+        {
+            x3 = x1 + x2;
+            x1 = x2;
+            x2 = x3;
+        }
+        return x3;
+    }
 }
