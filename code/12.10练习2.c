95cf678d62ac858f0996e387db3e8289f11f885d
tyf <1552068173@qq.com>
2020-12-10 18:53:40 +0800

    测试

--- code/12.10练习2.c
+++ code/12.10练习2.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-
+    int n, t, c = 0;
+    scanf("%d", &n);
+    if(n>=0)
+    {
+        t = n;
+        while(t>0)
+        {
+            c = c * 10 + t%10;
+            t /= 10;
+        }
+        printf("%d", c);
+    }
+    else
+    {
+        t = abs(n);
+        while(t>0)
+        {
+            c = c * 10 + t%10;
+            t /= 10;
+        }
+        printf("-%d", c);
+    }
+    
     return 0;
 }
