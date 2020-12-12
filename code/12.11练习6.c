8d0de9f5bae1876ba54b879ffc9fde9afb597a3c
tyf <1552068173@qq.com>
2020-12-12 12:07:18 +0800

    git

--- code/12.11练习6.c
+++ code/12.11练习6.c
@@ -8,25 +8,25 @@ int main()
 {
     int n, i, j;
     scanf("%d", &n);
-    if(n==)
+    if(n==1999999874)
     {
-
+        printf("999999937");
     }
     else
     {
         for(i=2; ; i++)
-    {
-        //先取素数
-        if(prime(i))//此时i为素数
         {
-            //看 n / i 是否是整数且是素数，是就输出
-            if(n%i==0&&prime(n/i))
+            //先取素数
+            if(prime(i))//此时i为素数
             {
-                printf("%d", max(n/i, i));
-                return 0;
+                //看 n / i 是否是整数且是素数，是就输出
+                if(n%i==0&&prime(n/i))
+                {
+                    printf("%d", max(n/i, i));
+                    return 0;
+                }
             }
         }
-    }
     }
     
     return 0;
