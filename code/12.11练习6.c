99e32b765766a5fa7545a2ccac0fbb5b6259f1ef
tyf <1552068173@qq.com>
2020-12-12 11:16:07 +0800

    12.12 11:16

---
+++ code/12.11练习6.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool prime(int);
+int max(int, int);
+
+int main()
+{
+    int n, i, j;
+    scanf("%d", &n);
+    if(n==)
+    {
+
+    }
+    else
+    {
+        for(i=2; ; i++)
+    {
+        //先取素数
+        if(prime(i))//此时i为素数
+        {
+            //看 n / i 是否是整数且是素数，是就输出
+            if(n%i==0&&prime(n/i))
+            {
+                printf("%d", max(n/i, i));
+                return 0;
+            }
+        }
+    }
+    }
+    
+    return 0;
+}
+
+bool prime(int x)
+{
+    int i;
+    for(i=2; i<x; i++)
+    {
+        if(x%i==0)
+        {
+            break;
+        }
+    }
+    if(i==x)
+    {
+        return true;
+    }
+    return false;
+}
+
+int max(int x, int y)
+{
+    if(x>=y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+    
+}
