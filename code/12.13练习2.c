11b9e380a33d156916eddaf4d301436a386d1a0f
tyf <1552068173@qq.com>
2020-12-13 22:19:10 +0800

    enter

--- code/12.13练习2.c
+++ code/12.13练习2.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* buildarr();
+void heapsort(int*);
+void displayarr(int*);
+
 int main()//堆排序_完全二叉树
 {
-    int n;
-    scanf("%d", &n);
-    int* a = (int*)malloc(sizeof(int) * n);
-    
+    int* a = buildarr();
+    heapsort(a);
+    displayarr(a);
     return 0;
+}
+
+int* buildarr()
+{
+    int n, i;
+    printf("要放几个元素？请输入:");
+    int* a = (int*)malloc(sizeof(int) * n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", a+i);
+    }
+    return a;
+}
+
+void heapsort(int* ac)
+{
+    
+    return;
+}
+
+void displayarr(int* ac)
+{
+    int len = sizeof(ac)/sizeof(int);
+    int i;
+    for(i=0; i<len; i++)
+    {
+        printf("%d", ac[i]);
+        if(i!=len-1)
+        {
+            printf(" ");
+        }
+    }
+    return;
 }
