ad47ccb8d826262510655ded4f1be631ed57a13a
tyf <1552068173@qq.com>
2020-12-13 21:37:59 +0800

    Enter

--- code/12.13练习1.c
+++ code/12.13练习1.c
@@ -2,6 +2,34 @@
 
 int main()
 {
-    
+    int a, b, i, j, t, tt;
+    scanf("%d %d", &a, &b);
+    for(i=a; i<=b; i++)
+    {
+        t = i;
+        tt = 0;
+        while(t>0)
+        {
+            tt = tt * 10 + t % 10;
+            t /= 10;
+        }
+        if(i==tt)
+        {
+            if(i%2!=0)
+            {
+                for(j=2; j<i; j++)
+                {
+                    if(i%j==0)
+                    {
+                        break;
+                    }
+                }
+                if(j==i)
+                {
+                    printf("%d\n", i);
+                }
+            }
+        }
+    }
     return 0;
 }
