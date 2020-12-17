f7d4b024772f932597b18d577ec81b14bddaf94c
tyf <1552068173@qq.com>
2020-12-17 19:57:03 +0800

    12.17 19：56

--- code/12.17汉诺塔.c
+++ code/12.17汉诺塔.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
-int TowerOfHanoi(int);
+void TowerOfHanoi(int, char, char, char);
 
 int main()
 {
     int n;
     printf("几层汉诺塔？请输入：");
     scanf("%d", &n);
-    printf("需要%d次移动", TowerOfHanoi(n));
+    TowerOfHanoi(n , 'A', 'B', 'C');
     return 0;
 }
 
-int TowerOfHanoi(int n)
+void TowerOfHanoi(int n, char cur, char tmp, char tgt)//这是三根柱子 他们是固定的。cur为起始柱子，tgt为目标柱，tmp为缓冲柱
 {
-    if(n==0)
+    if(n==1)
     {
-        return 0;
+        printf("将在%c柱子上的%d号盘挪到%c柱子\n", cur, 1, tgt);
+        return;
     }
-    return 2 * TowerOfHanoi(n-1) + 1; 
+    else
+    {
+        TowerOfHanoi(n-1, cur, tgt, tmp);
+        printf("将在%c柱子上的%d号盘挪到%c柱子\n",  cur, n, tgt);
+        TowerOfHanoi(n-1, tmp, cur, tgt);
+    }
 }
