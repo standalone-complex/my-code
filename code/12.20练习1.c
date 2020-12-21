ec75b2c8bef00b0fa69875d6184da19636687be8
tyf <1552068173@qq.com>
2020-12-21 20:46:48 +0800

    12.21 20:46

--- code/12.20练习1.c
+++ code/12.20练习1.c
@@ -5,7 +5,7 @@ void yanghui(int);
 
 int main()
 {
-    int n, i, j;
+    int n;
     scanf("%d", &n);
     yanghui(n);
     return 0;
@@ -17,6 +17,27 @@ void yanghui(int n)
     int** a = (int**)malloc(sizeof(int*) * n);
     for(i=0; i<n; i++)
     {
-        
+        a[i] = (int*)malloc(sizeof(int) * (i + 1));
+    }
+    for(i=0; i<n; i++)
+    {
+        a[i][0] = 1;
+        a[i][i] = 1;
+    }
+    for(i=2; i<n; i++)
+    {
+        for(j=1; j<i ; j++)
+        {
+            a[i][j] = a[i-1][j-1] + a[i-1][j];
+        }
+    }
+    
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<i+1; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
     }
 }
