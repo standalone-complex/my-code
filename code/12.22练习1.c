d3c117bfc8ebe4103ca6b6f2d9e4cb42178cc301
tyf <1552068173@qq.com>
2020-12-23 22:08:17 +0800

    12.23 22.08

---
+++ code/12.22练习1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int a[10], book[10], n;
+
+int main()//有种模拟的感觉...但不完全是
+{
+    
+    return 0; // return
+}
---
+++ code/12.23练习1.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+
+int Ack(int m, int n)
+{
+    if(m>0)
+    {
+        if(n==0)
+        {
+            return Ack(m-1, 1);
+        }
+        else
+        {
+            return Ack(m-1, Ack(m, n-1));
+        }
+        
+    }
+    else
+    {
+        return n-1;
+    }
+    
+
+}
+
+int f(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    if(n==1)
+    {
+        return 1;
+    }
+    return f(n-1) + f(n-2);
+}
+
+double fn(double x, int n)
+{
+    if(n==1)
+    {
+        return x;
+    }
+    return fn(x, n-1) + pow(-1, n-1)* pow(x, n);
+}
+
+void dectobin( int n )
+{
+    if(n == 0)
+        printf("0");
+    else if(n == 1)
+        printf("1");
+    else{
+        dectobin(n/2);//利用二进制的原理
+        printf("%d", n%2);
+    }
+}
+
+double fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    return n * f(n-1);
+}
+
+double factsum(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    if(n==1)
+    {
+        return fact(1);
+    }
+    return factsum(n-1) + fact(n);
+
+}
+
+double P(int n, double x)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    if(n==1)
+    {
+        return x;
+    }
+    return ((2*n-1)*P(n-1,x)-(n-1)*P(n-2,x))/n;
+}
+
+int sum(int n)
+{
+    if(n<=0)
+    {
+        return 0;
+    }
+    return sum(n-1) + n;
+}
+
+double calc_pow(double x, int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    return calc_pow(x, n-1) * x;
+}
+
+void printdigits(int n)
+{
+    if(n<10)
+    {
+        printf("%d\n", n);
+        return;
+    } 
+    printdigits(n/10);
+    printf("%d\n", n%10);
+}
+
+int search(int n)
+{
+    
+}
